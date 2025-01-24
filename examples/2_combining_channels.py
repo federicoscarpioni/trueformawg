814dbf333a793b11df5fdad8099b7b58cf8ef6f1
Federico Scarpioni <federicoscarpioni@gmail.com>
2025-01-24 16:28:27 +0100

    Update combine channels

--- examples/2_combining_channels.py
+++ examples/2_combining_channels.py
@@ -5,6 +5,6 @@ awgs = VISAdevices()
 trueform_address = awgs.list[1]
 # Configure channel 1
 awg_ch1 = TrueFormAWG(trueform_address,1)
-awg_ch1.combine_channels(1,2)
+awg_ch1.combine_channels()
 
 awg_ch1.set_indipendent()
