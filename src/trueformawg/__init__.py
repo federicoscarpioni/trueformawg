155996f0412733cd2a9f1dfab1ddb909aa3628b1
= <=>
2025-06-17 15:37:45 +0200

    Changed variable name

--- src/trueformawg/__init__.py
+++ src/trueformawg/__init__.py
@@ -24,10 +24,10 @@ class TrueFormAWG():
     '''
     
     
-    def __init__(self, address, channel):
+    def __init__(self, address, channel_num):
         # Initialize attributes
         self.address = address
-        self.channel = channel
+        self.channel = channel_num
         
         # Perform the connection
         self.rm = pyvisa.ResourceManager()
@@ -49,8 +49,8 @@ class TrueFormAWG():
         print('Available memory (in samples):' + self.device.query('SOURce1:DATA:VOLatile:FREE?'))
     
     def clear_ch_mem(self):
-        self.device.write(f':SOURce{self.channel}:DATA:VOLatile:CLEar')
-        print(f'Cleared memory of channel {self.channel}')
+        self.device.write(f':SOURce{self.channel_num}:DATA:VOLatile:CLEar')
+        print(f'Cleared memory of channel {self.channel_num}')
 
         
     def load_awf(self, AWFname, AWF):    
@@ -59,7 +59,7 @@ class TrueFormAWG():
         '''
         self.device.write('FORM:BORD NORM') # set the byte order
         print('Loading waveform to awg...')
-        bytes_sent = self.device.write_binary_values(f'SOUR{self.channel}:DATA:ARB {AWFname},', AWF, datatype='f', is_big_endian=True)
+        bytes_sent = self.device.write_binary_values(f'SOUR{self.channel_num}:DATA:ARB {AWFname},', AWF, datatype='f', is_big_endian=True)
         print(bytes_sent)
         self.device.write('*WAI') # Wait for the waveform to load
         print('Procedure complete.')
@@ -70,55 +70,55 @@ class TrueFormAWG():
         ''' Load waveform from internal storage to volatile memory of Trueform AWG'''
         
         # Turn on arb function
-        self.device.write(f':SOUR{self.channel}:FUNC %s' % ('ARB'))
+        self.device.write(f':SOUR{self.channel_num}:FUNC %s' % ('ARB'))
         # Select my arb from the internal memory of the instrument
-        self.device.write(f':SOUR{self.channel}:FUNC:ARB "%s"' % (AWFname))
+        self.device.write(f':SOUR{self.channel_num}:FUNC:ARB "%s"' % (AWFname))
     
     def avalable_memory(self):
-        print('Available memory (in samples): ', self.device.query(f'SOURce{self.channel}:DATA:VOLatile:FREE?'))
+        print('Available memory (in samples): ', self.device.query(f'SOURce{self.channel_num}:DATA:VOLatile:FREE?'))
     
     def print_errors(self):
         print(self.device.query('SYSTEM:ERROR?')) 
 
     
     def set_sample_rate(self, sampleRate):
-        self.device.write(':SOURce%d:FUNCtion:ARBitrary:SRATe %G' % (self.channel, sampleRate))
+        self.device.write(':SOURce%d:FUNCtion:ARBitrary:SRATe %G' % (self.channel_num, sampleRate))
 
 
     def set_amplitude(self, amplitude):
-        self.device.write(':SOURce%d:VOLTage %G' % (self.channel, amplitude)) 
+        self.device.write(':SOURce%d:VOLTage %G' % (self.channel_num, amplitude)) 
         
      
     def set_offset(self, offset):
-        self.device.write(':SOURce%d:VOLTage:OFFSet %G' % (self.channel, offset))
+        self.device.write(':SOURce%d:VOLTage:OFFSet %G' % (self.channel_num, offset))
         
         
     def set_Z_out_infinite(self):
-        self.device.write(':OUTPut%d:LOAD %s' % (self.channel, 'INF'))
+        self.device.write(':OUTPut%d:LOAD %s' % (self.channel_num, 'INF'))
 
     def set_Z_out(self, z_out):
-        self.device.write(':OUTPut%d:LOAD %s' % (self.channel, z_out))
+        self.device.write(':OUTPut%d:LOAD %s' % (self.channel_num, z_out))
 
     def combine_channels(self, source_channel = 'CH2'):
         '''
         Combine the signals digitally before sending to the DAC. Source channel
         must a be a string 'CH1' or 'CH2' (or 'NONE')
         '''
-        if self.channel == 1:
+        if self.channel_num == 1:
             pass
-        elif self.channel == 2:
+        elif self.channel_num == 2:
             source_channel = 'CH1'
-        self.device.write(':SOURce%d:COMBine:FEED %s' % (self.channel, source_channel))
+        self.device.write(':SOURce%d:COMBine:FEED %s' % (self.channel_num, source_channel))
 
     def set_indipendent(self):
-        self.device.write(':SOURce%d:COMBine:FEED %s' % (self.channel, 'NONE'))
+        self.device.write(':SOURce%d:COMBine:FEED %s' % (self.channel_num, 'NONE'))
 
     def turn_on(self):
-        self.device.write(':OUTPut%d %d' % (self.channel, 1)) 
+        self.device.write(':OUTPut%d %d' % (self.channel_num, 1)) 
         print('Awg turned on')
                         
     def turn_off(self):
-        self.device.write(':OUTPut%d %d' % (self.channel, 0)) 
+        self.device.write(':OUTPut%d %d' % (self.channel_num, 0)) 
         print('Awg turned off')
         
     def disconnect(self):
